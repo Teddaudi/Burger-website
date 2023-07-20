ba6ac80faa91c22f10be45b3c85509a73cbef84e
Teddaudi <daudited@gmail.com>
2023-07-20 22:44:30 +0300

    footer

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -15,27 +15,27 @@ const Footers = () => {
         <MDBFooter className='bg-dark text-center text-white mb-0 footer' id='footer'>
             <MDBContainer className='p-4 pb-0'>
                 <section className='mb-4'>
-                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://www.facebook.com/daudi.ted/' role='button'>
                         <MDBIcon fab icon='facebook-f' />
                     </MDBBtn>
-                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/daudi_ted/' role='button'>
                         <MDBIcon fab icon='instagram' />
                     </MDBBtn>
 
-                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/ted-daudi-177831194/' role='button'>
                         <MDBIcon fab icon='linkedin-in' />
                     </MDBBtn>
 
-                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://github.com/Teddaudi' role='button'>
                         <MDBIcon fab icon='github' />
                     </MDBBtn>
                 </section>
             </MDBContainer>
 
             <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                © 2020 Copyright:
-                <a className='text-white' href='https://mdbootstrap.com/'>
-                    MDBootstrap.com
+                © 2023 Copyright:
+                <a className='text-white' href='daudited@gmail.com'>
+                    daudited@gmail.com
                 </a>
             </div>
         </MDBFooter>

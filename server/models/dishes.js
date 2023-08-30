3da7ab7c5239fb39c866cc02f7111a0a1fc37fb5
Teddaudi <daudited@gmail.com>
2023-08-30 20:07:29 +0300

    removed the server file

--- server/models/dishes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-
-const Dishes =  new Schema({
-    title:{type:String,required:true},
-    price:{type:Number,required:true},
-    image:{type:String,required:true}
-})
-module.exports = mongoose.model('Dishes', Dishes)
